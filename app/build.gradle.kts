0f957f68fb3aea9de2ad658f82286b4e9d061681
Maksim Lukin <maximlm93@gmail.com>
2024-11-07 16:20:49 +0500

    project first initialization

---
+++ app/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("java")
+    application
+    checkstyle
+    jacoco
+}
+
+application {
+    mainClass = "hexlet.code.App"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.jacocoTestReport {
+    reports { xml.required.set(true) }
+}
